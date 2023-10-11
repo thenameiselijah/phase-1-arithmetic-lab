47b9c056b85c9720718724b2b2d5534b7b172786
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-10-11 18:09:54 -0400

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.ceil(Math.random() * 100); // generates a random integer between 1 and 100
+console.log(random);
+
+let num3 = 12 
+let num4 = 3
+
+let mod = num3 / num4;
+console.log(mod);
+
+let max = Math.max(5, 10, 15, 20);
